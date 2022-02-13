e4dad88c4c6ea6a2cde4d28ae187d6913f5038b8
Бороздин Дмитрий <borozdin1@yandex.ru>
2022-02-13 17:23:29 +0700

    Созданы компоненты для всех страниц приложения Main (/), Sign In (/login), MyList (/mylist), Film (/films/:id), Add review (/films/:id/review), Player (/player/:id). Создан комопнент для страницы 404. Установлен пакет react-router-dom. Добавлена настройка historyApiFallback: true. Описаны маршруты для компонентов страниц.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
+import NotFoundScreen from "../not-found-screen/not-found-screen";
 
 const App = (props) => {
   const {titleMovie} = props;
 
   return (
-    <Main titleMovie={titleMovie} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main titleMovie={titleMovie} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <Link to="/" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </Link>
+        </div>
+
+        <h1 className="page-title user-page__title">Sign in</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <form action="#" className="sign-in__form">
+          <div className="sign-in__fields">
+            <div className="sign-in__field">
+              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+            </div>
+            <div className="sign-in__field">
+              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+            </div>
+          </div>
+          <div className="sign-in__submit">
+            <button className="sign-in__btn" type="submit">Sign in</button>
+          </div>
+        </form>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <Link to="/" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </Link>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default SignIn;
