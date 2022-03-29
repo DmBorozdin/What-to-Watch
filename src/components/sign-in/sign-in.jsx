3ea9ce7c58f8a8f8034f19ae131ea8c010b14927
Дмитрий Бороздин <borozdin1@yandex.ru>
2022-03-29 11:40:26 +0000

    Обновлен компонент авторизации. С помощью хука useRef получается доступ к элементам формы и далее запись в хранилище в случае авторизации.

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useRef} from "react";
+import {useHistory} from "react-router-dom";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import {Link} from "react-router-dom";
+import {login} from "../../store/api-actions";
 
-const SignIn = () => {
+const SignIn = ({onSubmit}) => {
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -17,14 +35,22 @@ const SignIn = () => {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input
+                ref={loginRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
+                pattern= "^([^ ]+@[^ ]+\.[a-z]{2,6}|)$"
+                required/>
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <input ref={passwordRef} className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" required/>
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
@@ -51,4 +77,15 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+SignIn.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
+export {SignIn};
+export default connect(null, mapDispatchToProps)(SignIn);
